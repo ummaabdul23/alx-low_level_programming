78743b49a95dcfbeaac1a3c1cad27a6e278238a0
ummaabdul23 <zahrahani21@gmail.com>
2023-09-07 21:31:05 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,7 @@
   */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
-  return (0);
+  /*print the content of printf*/
+	printf("with proper grammar, but the outcome is a piece of art, ");
+	return (0);
 }
