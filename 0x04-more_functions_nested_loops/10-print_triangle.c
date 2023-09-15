99223b5b9c405a33a4583c5580ee1a55e03ef309
ummaabdul23 <zahrahani21@gmail.com>
2023-09-15 01:34:50 +0000

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,17 +9,24 @@
 void print_triangle(int size)
 {
 	int i, j;
-
-        if (size <= 0)
-        {
-                _putchar('\n');
-        }
-for (i = 0; i <= size; i++)
-{
-for (j = 0; j < i; j++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - 1; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
