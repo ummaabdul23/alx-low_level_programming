986a6ec1bd6f122d62aaab17e87079b009124f1c
ummaabdul23 <zahrahaani21@gmail.com>
2023-09-15 23:29:28 +0000

    2-largest_number.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,7 +23,6 @@ void print_remaining_days(int month, int day, int year)
         printf("Remaining days: %d\n", 366 - day);
     }
     else
-	    A
     {
         if (month == 2 && day == 60)
         {
