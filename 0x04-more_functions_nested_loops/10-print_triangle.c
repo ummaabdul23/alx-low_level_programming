9b11c361d3377074ad11f8a07e09cf306d8042ae
ummaabdul23 <zahrahaani21@gmail.com>
2023-09-16 09:38:21 +0000

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,14 +15,14 @@ void print_triangle(int size)
 	else
 	{
 		int i, j;
-		
+
 		for (i = 1; i <= size; i++)
 		{
 			for (j = 1; j <= size; j++)
 			{
 				if (j <= size - i)
 				{
-				_putchar(' ');
+					_putchar(' ');
 				}
 				else
 				{
