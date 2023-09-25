f1a914a5ee5be5094e734183061574e538ad741e
ummaabdul23 <zahrahaani21@gmail.com>
2023-09-25 15:34:24 +0000

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,14 @@
   * Return: char
   */
 
-  char *_memcpy(char *dest, char *src, unsigned int n)
-  {
-	  unsigned int i = 0;
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
 
-	  while (i < n)
-	  {
-		  dest[i] = src[i];
-		  i++;
-	  }
-	  return (dest);
-  }
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+		}
+		return (dest);
+		}
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,22 +8,22 @@
   * Return: char
   */
 
-  char *_strchr(char *s, char c)
-  {
-	  if (*s == '\0')
-	  {
-		  return (s);
-	  }
-	  while (*s)
-	  {
-		  if (*s == c)
-		  {
-			  return (s);
-		  }
-		  s++;
-	  }
-	  if (c == '\0')
-	  {
-		  return (s);
-		  return ('\0');
-	  }
+char *_strchr(char *s, char c)
+{
+	if (*s == '\0')
+	{
+		return (s);
+		}
+		while (*s)
+		{
+			if (*s == c)
+			{
+				return (s);
+				}
+				s++;
+				}
+				if (c == '\0')
+				{
+					return (s);
+					return ('\0');
+					}
