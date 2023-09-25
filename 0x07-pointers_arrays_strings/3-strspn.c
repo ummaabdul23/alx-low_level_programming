38fa08fab0dddaedb4a8048fc540367b3405223b
ummaabdul23 <zahrahaani21@gmail.com>
2023-09-25 15:36:40 +0000

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,18 +8,18 @@
   * Return: unsigned int
   */
 
-  unsigned int _strspn(char *s, char *accept)
-  {
-	  unsigned int count = 0;
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
 
-	  for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
-	  {
-		  for (j = 0; accept[j] != '\0'; j++)
-		  {
-			  if (s[i] == accept[j])
-				  count++;
-		  }
-		  j = 0;
-	  }
-	  return (count);
-  }
+	for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				count++;
+				}
+				j = 0;
+				}
+				return (count);
+				}
