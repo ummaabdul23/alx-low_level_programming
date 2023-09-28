579ba584b64a8fc689469a6704fc81faacb2d2fa
ummaabdul23 <zahrahaani21@gmail.com>
2023-09-28 13:35:36 +0000

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -15,12 +15,4 @@ void _print_rev_recursion(char *s)
 	_print_rev_recursion(s);
 	s--;
 	_putchar(*s);
-}/*
-void _print_rev_recursion(char *s)
-{
-	if (s[0] != '\0')
-	{
-		_print_rev_recursion(s - 1);
-		_putchar(s[0]);
-	}
-}*/
+}
