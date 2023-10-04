191d1bca2f8c72f6f22ecee44404cce8c649e8e5
ummaabdul23 <zahrahaani21@gmail.com>
2023-10-04 23:22:26 +0000

    101-strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,22 +10,35 @@
   */
 char **strtow(char *str)
 {
-	char *a = NULL;
-	unsigned int x = 0, y = 0, z;
+	char **d;
+	int i;
+	int j = 0;
+	int con = 0;
 
-	if (strncmp(str, "", 1) || str == NULL)
+	if (str == NULL)
 		return (NULL);
-	a = malloc((x + y + 1) * sizeof(char));
-	if (a == NULL)
-		return (NULL);
-	for (z = 0; z < x; z++)
-		a[z] = str[z];
-	x = z;
-	for (z = 0; z < y; z++)
+
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		a[x] = str[z];
-		x++;
+		if (str[i] != 32)
+			con++;
 	}
-	a[x] = '\0';
-	return (NULL);
+
+	d = malloc(sizeof(char) * con);
+
+	if (d == NULL)
+		return (NULL);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] != 32)
+		{
+			*d[j] = str[i];
+			j++;
+		}
+		else
+		{
+		}
+	}
+	return (d);
 }
