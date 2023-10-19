f6f40850ef2d8f4188cc094bceb3254aae73dc21
ummaabdul23 <zahrahaani21@gmail.com>
2023-10-19 22:49:10 +0000

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,23 @@
  section .data
  	hello db "Hello, Holberton", 10, 0
+	format db "%s", 0
+
  section .text
  	global _start
 	extern printf
 
 _start:
-	push rdi
-	lea rdi, [hello]
-	call printf
-	pop rdi
+	push rbp
+	mov rbp, rsp
 
-	mov rax, 60
-	xor rdi, rdi
-	syscall
+	mov rdi,format
+	mov rsi, hello
+	mov rax, 0
+
+	call printf
+
+	pop rbp
+
+	mov rax, 0
+	ret
 
