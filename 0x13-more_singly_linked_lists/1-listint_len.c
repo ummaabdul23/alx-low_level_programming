ae36578dc842c1f934b924d326fba714b496c490
ummaabdul23 <zahrahaani21@gmail.com>
2023-10-23 21:49:47 +0000

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  * listint_len - returns the number of elements in a linked lists
+  * @h: linked list parameter
+  *
+  * Return: count
+  */
+
+size_t listint_len(const listint_t *h)
+{
+    size_t count = 0;
+
+    while (h)
+    {
+        count++;
+        h = h->next;
+    }
+
+    return (count);
+}
