c6d6615e333687185328fb2e750d608c86f84ccd
ummaabdul23 <zahrahaani21@gmail.com>
2023-10-26 22:34:04 +0000

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at an index
+ * @n: number parameter
+ * @index: the given index
+ * Return: the value at an index or -1
+ */
+
+int get_bit(unsigned long int n, unsigned int index) {
+    unsigned int size = sizeof(n) * 8;
+    unsigned long int mask = 1UL << index;
+
+    if (index >= size) {
+        return (-1);
+    }
+    if ((n & mask) == mask) {
+        return (1);
+    } else {
+        return (0);
+    }
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: number parameter
+ * @index: the given index
+ * Return: 1 or -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index) {
+    unsigned int size = sizeof(*n) * 8;
+    unsigned long int mask = ~(1UL << index);
+
+    if (index >= size) {
+        return -1;
+    }
+    *n &= mask;
+
+    return (1);
+}
