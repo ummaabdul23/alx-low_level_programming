41880bd401f52c80f1bfa66d63e9f29272a924f9
ummaabdul23 <zahrahaani21@gmail.com>
2023-10-26 23:02:33 +0000

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,28 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary to unsigned int
- * @b: pointer to a string of 0 and 1 chars
- * return: the converted nymber or 0
- */
+  * binary_to_uint - converts a binary to unsigned int
+  * @b: pointer to a string of 0 and 1 chars
+  * Return: the converted nymber or 0
+  */
 
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result;
 
-	if (b == NULL) {
+	if (b == NULL)
 		return (0);
-	}
 	result = 0;
 
-	while (*b) {
-		if (*b == '1') {
+	while (*b)
+	{
+		if (*b == '1')
 			result = (result << 1) | 1;
-		} else if (*b == '0') {
+		else if (*b == '0')
 			result = result << 1;
-		} else {
+		else
 			return (0);
-		}
 		b++;
 	}
-
 	return (result);
 }
